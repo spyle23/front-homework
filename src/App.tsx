6a1d8b914aff6f539e7b3daea719a1a042521b49
spyle23 <sampingyen.lee@gmail.com>
2023-01-11 11:42:42 +0300

    first test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { StylesProvider } from "@mui/styles"
+import { theme } from "./theme";
+import { MainRouter } from "./router/MainRouter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <StylesProvider injectFirst>
+          <CssBaseline />
+          <MainRouter />
+        </StylesProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export const Home = () => {
+  return <Container></Container>;
+};
