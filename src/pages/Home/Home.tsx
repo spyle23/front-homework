569c2d9e3015ae00aff6d5e175c434af9c7618ed
spyle23 <sampingyen.lee@gmail.com>
2023-01-11 18:09:48 +0300

    update state

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,12 @@ import { Container, Typography } from "@mui/material";
 import { SearchKeyword } from "../../components/Input/SearchKeyword";
 import axios from "axios";
 import { PLACE_QUERY } from "../../API/places/query";
+import { Place } from "../../interfaces/Place";
 
 export const Home = React.memo(() => {
   const [search, setSearch] = useState<string>("");
   const [position, setPosition] = useState<string>("");
+  const [places, setPlaces] = useState<Place[]>([]);
   const handleChange = (
     e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
   ) => {
@@ -25,22 +27,25 @@ export const Home = React.memo(() => {
     });
   };
 
-  const getVenues = () => {
+  const getVenues = async () => {
     const params = {
       //   type: "place",
       ll: position,
       client_id: process.env.REACT_APP_CLIENT_ID || "",
       client_secret: process.env.REACT_APP_CLIENT_SECRET || "",
       query: search,
-      //   oauth_token: process.env.REACT_APP_API_KEY,
+      oauth_token: process.env.REACT_APP_API_KEY,
       v: "20221016",
     };
-
-    PLACE_QUERY.searchPlace(params).then((data) => console.log(data));
+    const data = await PLACE_QUERY.searchPlace(params);
+    if (data) {
+      console.log(data);
+      setPlaces([...data]);
+    }
   };
 
   const handleSearch = async () => {
-    getVenues();
+    await getVenues();
   };
   return (
     <Container>
